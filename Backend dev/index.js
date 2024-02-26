bc48a7afe6a6c59b9c346ce9049c0e84e064bfdd
Mohammad Abdur Rakib <77808270+marakib991@users.noreply.github.com>
2024-02-26 07:37:28 -0500

    Update index.js

--- Backend dev/index.js
+++ Backend dev/index.js
@@ -2,9 +2,7 @@ import app from "./server.js"
 import mongodb from "mongodb"
 import ReviewsDAO from "./dao/reviewsDAO.js"
 const MongoClient = mongodb.MongoClient
-const mongo_username = process.env['MONGO_USERNAME']
-const mongo_password = process.env['MONGO_PASSWORD']
-const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.3tzbhom.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://rarefigure:HR547s9c4lSnCQLa@cluster0.3tzbhom.mongodb.net/?retryWrites=true&w=majority`
 
 const port = 8000
 await MongoClient.connect(
